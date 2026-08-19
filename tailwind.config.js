dd90eb2b3022ad05731866c52b49f22e69ff9a5e
Srinidhi Kotteswaran <srinidhikotteswaran12@gmail.com>
2026-08-19 18:22:47 -0400

    Document Tailwind content scanning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // Scan the app shell and all source components for utility classes.
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
